5bd41b47e136ce21ca978ffaabd77d23f2fb6c14
--global <--global>
2021-07-19 10:47:13 +0300

    Инициализация базового модуля для работы с клавиатурой

--- core/Base/BaseHandler.py
+++ core/Base/BaseHandler.py
@@ -1,8 +1,10 @@
 from Core.Base.BaseRedis import BaseRedis
+from Core.Base.BaseKeyboard import BaseKeyboard
 
 
 class BaseHandler():
     redis: BaseRedis
 
     def __init__(self):
-        self.redis = BaseRedis
+        self.redis = BaseRedis()
+        self.keyboard = BaseKeyboard()
